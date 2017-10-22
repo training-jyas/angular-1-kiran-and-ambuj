2e3b4db1aa3817ca928c52bfb8dbe1966b212c2e
Jyasveer Gotta <jyasveer@gmail.com>
2017-10-22 12:33:45 +0530

    delete files

--- angular-app/src/main.js
+++
@@ -1,9 +0,0 @@
-console.log('angular is loaded', angular);
-
-// here we are creating our main module
-var app = angular.module('app', []);
-
-// here we are creating a controller for module `app`
-app.controller('appController', ['$scope', function($scope){
-    $scope.person = 'RTST';
-}]);
